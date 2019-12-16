a2eaca336785519a59791a09d596bcf7200edd4b
Tali Avni <tali@aristo7.com>
2019-12-15 21:15:00 -0800

    travis

--- challenges/array-shift/array-shift.js
+++ challenges/array-shift/array-shift.js
@@ -1,34 +1,34 @@
 function insertIntoArray(array, value) {
-    const newArray = [];
-    const midpoint = Math.ceil((array.length) / 2);
+  const newArray = [];
+  const midpoint = Math.ceil((array.length) / 2);
 
 
-    for(let i = 0; i < array.length; i++) {
-        if(i < midpoint) {
-            newArray[i] = array[i];
-        } else if(i === midpoint) {
-            newArray[i] = value;
-        } else if(i > midpoint) {
-            newArray[i] = array[i - 1];
-        }
+  for(let i = 0; i < array.length; i++) {
+    if(i < midpoint) {
+      newArray[i] = array[i];
+    } else if(i === midpoint) {
+      newArray[i] = value;
+    } else if(i > midpoint) {
+      newArray[i] = array[i - 1];
     }
-    return newArray;
+  }
+  return newArray;
 }
 
 const removeMiddle = (array) => {
-    const newArray = [];
-    const midpoint = Math.floor((array.length) / 2);
+  const newArray = [];
+  const midpoint = Math.floor((array.length) / 2);
 
-    for(let i = 0; i < array.length - 1; i++){
-        if(i < midpoint){
-            newArray[i] = array[i];
-        }
-        if(i >= midpoint){
-            newArray[i] = array[i + 1];
-        }
+  for(let i = 0; i < array.length - 1; i++){
+    if(i < midpoint){
+      newArray[i] = array[i];
     }
+    if(i >= midpoint){
+      newArray[i] = array[i + 1];
+    }
+  }
 
-    return newArray;
+  return newArray;
 };
 
 module.exports = { insertIntoArray, removeMiddle };
--- challenges/arrayReverse/array-reverse.js
+++ challenges/arrayReverse/array-reverse.js
@@ -1,19 +1,19 @@
 function reverseArray(array) {
-    for(var i = 0; i <= Math.floor((array.length - 1) / 2); i++) {
-        let el = array[i];
-        array[i] = array[array.length - 1 - i];
-        array[array.length - 1 - i] = el;
-    }
-    return array;
+  for(var i = 0; i <= Math.floor((array.length - 1) / 2); i++) {
+    let el = array[i];
+    array[i] = array[array.length - 1 - i];
+    array[array.length - 1 - i] = el;
+  }
+  return array;
 }
 
 
 function reverseArrayAgain(array) {
-    var newArray = [];
-    for(var i = array.length - 1; i >= 0; i--) {
-        newArray.push(array[i]);
-    }
-    return newArray;
+  var newArray = [];
+  for(var i = array.length - 1; i >= 0; i--) {
+    newArray.push(array[i]);
+  }
+  return newArray;
 }
 
 let testArray = [1, 2, 3, 4, 5];
