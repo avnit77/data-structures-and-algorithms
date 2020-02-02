08d2cc1214f70dab4f3c78a92b3b7637b5e78b1c
Tali Avni <tali@aristo7.com>
2020-02-01 18:02:39 -0800

    tests passing on tree

---
+++ challenges/tree/tree.js
@@ -0,0 +1,73 @@
+class Node {
+  constructor(data){
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinaryTree {
+  constructor(){
+    this.root = null;
+  }
+
+  inOrder(node){
+    let array = [];
+    if(node){
+      array = array.concat(this.inOrder(node.left));
+      array.push(node.data);
+      array = array.concat(this.inOrder(node.right));
+    }
+    return array;
+  }
+
+  preOrder(node){
+    let array = [];
+    if(node != null){
+      array.push(node.data);
+      array = array.concat(this.inOrder(node.left));
+      array = array.concat(this.inOrder(node.right));
+    }
+    return array;
+  }
+
+  postOrder(node){
+    let array = [];
+    if(node !== null){
+
+      array = array.concat(this.postOrder(node.right));
+      array.push(node.data);
+      array = array.concat(this.postOrder(node.left));
+    }
+    return array;
+  }
+
+  contains(data){
+    const array = this.inOrder(this.root);
+    if(array.includes(data)){
+      return true;
+    }
+    else return false;
+  }
+
+  add(data){
+    var newNode = new Node(data);
+    if(this.root === null){
+      this.root = newNode;
+    } else this.insertNode(this.root, newNode);
+  }
+
+  insertNode(node, newNode)
+  {
+    if(newNode.data < node.data)
+    { if(node.left === null){
+      node.left = newNode;
+    } else this.insertNode(node.left, newNode);
+    } else { if(node.right === null){
+      node.right = newNode;
+    } else this.insertNode(node.right, newNode);
+    }
+  }
+}
+
+module.exports = BinaryTree;
